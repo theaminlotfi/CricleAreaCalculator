35d4222cd33b063426b52e56c05175da9ce855c8
AminLotfi <theaminlotfi2003@gmail.com>
2023-10-31 11:03:16 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CricleAreaCalculator
+{
+    internal class Program
+    {
+        public static double CricleAreaCalculator(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        public static double GetNumberFromUser()
+        {
+            Console.WriteLine("Enter the radius of the circle:");
+            var numStr = Console.ReadLine();
+            if (string.IsNullOrEmpty(numStr))
+            {
+                return 0;
+            }
+            numStr = numStr.Trim();
+            double radius = double.Parse(numStr);
+            return radius;
+        }
+        public static void Main(string[] args)
+        {
+            double radius = GetNumberFromUser();
+            Console.WriteLine("Radius = " + radius);
+            double area = CricleAreaCalculator(radius);
+            Console.WriteLine("The area of the circle is: " + area);
+        }
+    }
+}
